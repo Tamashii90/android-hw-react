938be396eb34589829b635ba8c246162e2bedb29
Tamashii90 <patoboy90@hotmail.com>
2021-10-15 23:03:23 +0300

    prevent list from persisting between navigations

--- src/context/ListContext.jsx
+++ src/context/ListContext.jsx
@@ -1,9 +1,15 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
+import { useLocation } from "react-router";
 
 const ListContext = createContext();
 
 export function ListContextProvider({ children }) {
 	const [list, setList] = useState([]);
+	const location = useLocation();
+	useEffect(() => {
+		// prevents list from persisting between navigations
+		setList([]);
+	}, [location]);
 	return (
 		<ListContext.Provider value={[list, setList]}>
 			{children}
