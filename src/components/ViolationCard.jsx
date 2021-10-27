4b23519ee7f53d396bf9b2d54245f33603b27ebe
Tamashii90 <patoboy90@hotmail.com>
2021-10-27 22:54:31 +0300

    change violation card style

--- src/components/ViolationCard.jsx
+++ src/components/ViolationCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import ErrorSvg from "./ErrorSvg";
 
 export default function ViolationCard({ card }) {
 	const history = useHistory();
@@ -8,18 +9,21 @@ export default function ViolationCard({ card }) {
 	};
 	return (
 		<div className="col-md-6 my-3" onClick={navigateToCard}>
-			<div className="violation-card bg-primary p-4 mx-1">
-				<div className="d-flex justify-content-between mb-3">
-					<span>{card.plugedNumber}</span>
-					<span>{card.driver}</span>
-				</div>
-				<div className="d-flex justify-content-between mb-3">
-					<span>{card.location}</span>
-					<span>{card.date}</span>
-				</div>
-				<div className="d-flex justify-content-between mb-3">
-					<span>{card.paid}</span>
-					<span>{"$" + card.tax}</span>
+			<div className="violation-card bg-primary p-3 mx-1">
+				<div className="d-flex">
+					<ErrorSvg width="100px" />
+					<div className="ms-2 d-flex flex-column flex-grow-1 justify-content-between">
+						<div className="d-flex justify-content-between">
+							<span>{card.driver}</span>
+							<span>{card.date}</span>
+						</div>
+						<div className="d-flex justify-content-between">
+							<span>{card.location}</span>
+							<span>{"$" + card.tax}</span>
+						</div>
+						{/* <div className="d-flex justify-content-between mb-3">
+						</div> */}
+					</div>
 				</div>
 			</div>
 		</div>
